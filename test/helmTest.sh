89a8805a55b964b215c30ad979a7af202c2398a7
hsimjee <huzaifah@shoreline.io>
2022-07-25 14:13:48 -0700

    add tolerations to chart

---
+++ test/helmTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# bash helmTest.sh {1: daemonset / 2: deployment} 
+
+TYPE=${1}
+if [ ${TYPE} -eq 1 ]
+then
+  FILE="values_daemonset.yaml"
+elif [ ${TYPE} -eq 2 ]
+then
+  FILE="values_deployment.yaml"
+fi
+
+helm template --debug shoreline-agent ../shoreline-agent -f ${FILE}
